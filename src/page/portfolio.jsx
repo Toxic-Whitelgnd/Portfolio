e0d7044ad4e58e723ac438b11286c2d6a8bcceee
Toxic-Whitelgnd <whitelegend56@gmail.com>
2022-07-05 18:25:25 +0530

    By-ToxicWhitelgnd

--- src/page/portfolio.jsx
+++ src/page/portfolio.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "../styles/portfolistyle.css"
 import img1 from "../images/tarun right.png"
+// import { Typewriter, useTypewriter, Cursor } from 'react-simple-typewriter'
+import Typewriter from 'typewriter-effect';
 
 export default function Portfolio(){
     return(
@@ -8,11 +10,23 @@ export default function Portfolio(){
         <div className="box">
             <div className="content">
                 <img src={img1} ></img>
-                <div className="name">Tarun S K<br></br></div><div className="skill"><span>Web Developer | Frontend</span></div>
+                <div className="name">Tarun S K<br></br></div>
+
+                <div className="skill">
+                <Typewriter
+                options={{
+                    strings: ['Student','Coder','Frontend','Mobile App Developer','Gamer','Software Development'],
+                    autoStart: true,
+                    loop: true,
+                    cursor: '#',
+                    deleteSpeed: 40,
+                    
+                }}
+                />
+            </div>
                 <a href="https://toxic-whitelgnd.github.io/portfolio-deploy/">Learn More</a>
             </div>
-        </div>
-        
+        </div>     
         </>
     )
 }
